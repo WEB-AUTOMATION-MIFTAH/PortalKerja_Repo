4de34becbd80e3bafa5580ef68cd1bdbb69a8d4c
Miftah Ramadhan Personal <miftahramadhan90@gmail.com>
2024-04-07 01:10:38 +0700

    update 17 - add pytest.ini

--- Test/custom_pytest_html_plugin.py
+++ Test/custom_pytest_html_plugin.py
@@ -2,4 +2,4 @@ import datetime
 from _pytest.config import Config
 
 def pytest_configure(config: Config):
-    config.html_report_title = f"Report generated on {datetime.datetime.now() + datetime.timedelta(hours=7):%d-%b-%Y at %H:%M:%S} (UTC+7) by pytest-html {config.pluginmanager.get_plugin('pytest-html').version}"
+    config.html_report_title = f"Report generated ccc on {datetime.datetime.now() + datetime.timedelta(hours=7):%d-%b-%Y at %H:%M:%S} (UTC+7) by pytest-html {config.pluginmanager.get_plugin('pytest-html').version}"
