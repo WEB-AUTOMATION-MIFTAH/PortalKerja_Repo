2335979323cb323378b32a4903921c579f1b6ad1
Miftah Ramadhan Personal <miftahramadhan90@gmail.com>
2024-04-07 02:04:41 +0700

    chane jkt time final

--- custom_pytest_html_plugin.py
+++
@@ -1,5 +0,0 @@
-import datetime
-from _pytest.config import Config
-
-def pytest_configure(config: Config):
-    config.html_report_title = f"Report generated ccc on {datetime.datetime.now() + datetime.timedelta(hours=7):%d-%b-%Y at %H:%M:%S} (UTC+7) by pytest-html {config.pluginmanager.get_plugin('pytest-html').version}"
