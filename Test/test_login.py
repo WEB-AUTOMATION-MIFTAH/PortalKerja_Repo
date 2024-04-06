33a468184900e091cc71a30626a10a9172fbda9d
Miftah Ramadhan Personal <miftahramadhan90@gmail.com>
2024-04-06 12:19:37 +0700

    add report1 file

--- Test/test_login.py
+++ Test/test_login.py
@@ -4,5 +4,6 @@ from Test.object_instance import ObjectInstantiation
 @pytest.mark.usefixtures("setup_scope_class")
 class TestFooterUi(ObjectInstantiation):
 
+    @pytest.mark.smoke
     def test_verify_theres_copyright(self):
         self.footer().check_copyright_is_visible()
