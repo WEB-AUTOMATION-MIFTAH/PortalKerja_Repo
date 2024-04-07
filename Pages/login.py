003b1ea0ab55cf8bd1633a2ba84bd08c35553e73
Miftah Ramadhan Personal <miftahramadhan90@gmail.com>
2024-04-07 13:47:33 +0700

    get locator and gitignore file

--- Pages/login.py
+++ Pages/login.py
@@ -5,3 +5,10 @@ class LocatorLoginPage:
     LOC_EMAIL_FIELD = (By.XPATH, '//input[@id="login"]')
     LOC_PWD_FIELD = (By.XPATH, '//input[@id="password"]')
     LOC_LOGIN_BTN = (By.XPATH, '//div[@class="modal-body"]/form/div[5]')
+
+    LOC_TXT_TITLE_MDL = (By.XPATH, '//div[@class="modal-body"]/form/div/h4]')
+    LOC_TXT_SUBTIT_1_MDL = (By.XPATH, '//div[@class="modal-body"]/form/div[1]/div/span/text()[1]')
+    LOC_TXT_SUBTIT_2_MDL = (By.XPATH, '//div[@class="modal-body"]/form/div[1]/div/span/text()[2]')
+    LOC_IMG_SUBTIT_1_MDL = (By.XPATH, '//div[@class="modal-body"]/form/div[1]/div/span/img[1]')
+    LOC_IMG_SUBTIT_2_MDL = (By.XPATH, '//div[@class="modal-body"]/form/div[1]/div/span/img[2]')
+
